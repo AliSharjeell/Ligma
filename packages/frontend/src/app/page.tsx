277542180da9ea5a62514fae18c7365653a4811d
AliSharjeell <ali.sharjeel2014@gmail.com>
2026-04-29 00:07:33 +0500

    feat: replace home page with WelcomeDialog

--- packages/frontend/src/app/page.tsx
+++ packages/frontend/src/app/page.tsx
@@ -1,26 +1,7 @@
 'use client';
 
-import React from 'react';
-import { SocketProvider } from '@/contexts/socket-context';
-import { InfiniteCanvas } from '@/components/canvas/InfiniteCanvas';
-import { Toolbar } from '@/components/toolbar/Toolbar';
-import { PresenceHeatmap } from '@/components/canvas/PresenceHeatmap';
-import { PresenceZones } from '@/components/canvas/PresenceZones';
-import { TimeTravel } from '@/components/canvas/TimeTravel';
-
-const WS_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3001';
+import { WelcomeDialog } from '@/components/WelcomeDialog';
 
 export default function Home() {
-  const roomId = 'default';
-  return (
-    <SocketProvider url={WS_URL} canvasId={roomId}>
-      <main className="h-screen w-screen relative overflow-hidden bg-white">
-        <InfiniteCanvas />
-        <Toolbar />
-        <PresenceHeatmap />
-        <PresenceZones />
-        <TimeTravel />
-      </main>
-    </SocketProvider>
-  );
+  return <WelcomeDialog />;
 }
